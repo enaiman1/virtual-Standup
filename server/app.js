c95891c532681bfafa70fffd11b6673d8ff80698
Ericcentrik <enaiman1@gmail.com>
2020-04-21 21:28:06 -0400

    created inital folder and app.js

---
+++ server/app.js
@@ -0,0 +1,32 @@
+// dependencies
+const express = require('express')
+const api = require('./api')
+const morgan = require('morgan') //logger
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// Setting up PORT
+app.set('port', (process.env.PORT || 8081))
+
+// Creating express app and configuring middleware needed for authentication
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+app.use('/api', api)
+app.use(express.static('static'))
+
+app.use(morgan('dev'))
+
+// this function set up a reusable error if user goes to wrong route
+app.use(function (req, res){
+    const err = new Error('Not Found')
+    err.status = 404
+    res.json(err)
+})
+
+app.listen(app.get('port'), function () {
+	console.log(`==> 🌎  Listening on port: ${app.get('port')}!`)
+})
+
